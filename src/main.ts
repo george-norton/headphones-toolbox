194744cc1a49bcfcb852deb09221fb7102eff2b1
George Norton <george_norton_uk@hotmail.com>
2023-06-04 01:23:32 +0100

    Fix build error.

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,17 @@
-import { createApp } from 'vue';
-import { Quasar, Notify } from 'quasar';
+import { createApp } from 'vue'
+import { Quasar, Notify } from 'quasar'
 import 'quasar/src/css/index.sass'
-import App from '@/App.vue';
+import App from '@/App.vue'
 import '@quasar/extras/material-icons/material-icons.css'
-import { invoke } from '@tauri-apps/api'
 
 createApp(App)
     .use(Quasar, {
-        plugins: [
+        plugins: {
             Notify
-        ],
+        },
         config: {
             notify: {
-                position: 'bottom',
+                position: 'bottom'
             },
         },
     }).mount("#tauri-app")
